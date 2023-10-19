5b7459b7b5bc8fdaa7d0c53ea2c9d78f46261fda
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-19 16:25:14 +0530

    updated post controllers, model

--- blog/Model/Post.js
+++ blog/Model/Post.js
@@ -4,15 +4,21 @@ const PostSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        trim: true,
         maxlength: 100
     },
     body: {
         type: String,
         required: true,
-        trim: true,
         maxlength: 1000
-    }
+    },
+    likes:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Like'
+    }],
+    Comments:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
 });
 
 module.exports = mongoose.model('Post', PostSchema);
--- blog/controlles/getPost.js
+++ blog/controlles/getPost.js
@@ -2,12 +2,12 @@ const post = require("../Model/Post")
 exports.getpost = async(req,res)=>{
     try {
         //fetch all todos from db
-        const todos= await post.find({});
+        const posts= await post.find({}).populate('Comments');
 
         //response with success
         res.status(200).json({
             success:true,
-            data:todos,
+            data:posts,
             message:"All post fetched successfully"
         })
     } catch (error) {
