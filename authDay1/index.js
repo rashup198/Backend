c3be158bfb39106f5397b5997abcadb575df5c31
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-21 01:11:33 +0530

    updated

---
+++ authDay1/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT ;
+
+//middelware to parse json data
+app.use(express.json());
+
+// call the db
+const db = require("./config/database");
+db.connect();
+
+//import the routes and mount
+const user = require("./routes/user");
+app.use("/api/v1",user);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
