a2fb2c9f6e1e7759ca39db48b12fc1969cb967d1
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-21 02:35:08 +0530

    updated login

--- auth/controllers/auth.js
+++ auth/controllers/auth.js
@@ -1,6 +1,7 @@
 const bcypt = require('bcrypt');
 const user= require('../model/user');
-
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 // signup controller
 
 exports.signup = async (req,res) => {
@@ -30,8 +31,7 @@ exports.signup = async (req,res) => {
         })
         return res.status(201).json({
             success:true,
-            message:"User created successfully",
-            
+            message:"User created successfully",         
         })
 
     } catch (error) {
@@ -40,5 +40,66 @@ exports.signup = async (req,res) => {
             success:false,
             message:"user creation failed please try again"
         })
+    }
+}
+
+// login controller
+
+exports.login = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+        // check if email and password is provided
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please provide email and password"
+            })
+        }
+        // check if user exists
+        const userExists = await user.findOne({email});
+        if(!userExists){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid credentials"
+            })
+        }
+            const payload={
+                email:userExists.email,
+                id:userExists._id,
+                role:userExists.role
+            }
+        // check if password matches and create token
+        if(await bcypt.compare(password,userExists.password)){
+            let token= jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {expiresIn:"1d"})
+                
+            userExists.toObject();    
+            userExists.token=token;
+            userExists.password=undefined;
+            const options={
+                expires:new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true
+            }
+            res.cookie(
+                "token",token,options
+            ).status(200).json({
+                success:true,
+                message:"User logged in successfully",
+                data:userExists,token
+            })
+        }
+        else{
+            return res.status(400).json({
+                success:false,
+                message:"Invalid credentials"
+            })
+        }
+    } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+        success:false,
+        message:"Something went wrong"
+    })
     }
 }
