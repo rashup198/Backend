4f60a36721193c009a3b7008072cc00816c6eae8
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-22 20:46:48 +0530

    updated

--- auth/index.js
+++ auth/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT ;
+const cookieParser = require('cookie-parser');
 
 //middelware to parse json data
 app.use(express.json());
+app.use(cookieParser());
 
 // start the db
 const db = require("./config/database");
@@ -12,6 +14,7 @@ db.connect();
 
 //import the routes and mount
 const user = require("./routes/user");
+const cookieParser = require('cookie-parser');
 app.use("/api/v1",user);
 
 app.listen(port, () => {
