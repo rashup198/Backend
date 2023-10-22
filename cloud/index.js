7bc6417fa0b0f9fdb5b5506979dca9ba0a7f6fda
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-22 23:31:31 +0530

    updated Index.js

---
+++ cloud/index.js
@@ -0,0 +1,25 @@
+ const express = require('express');
+
+const app = express();
+require("dotenv").config();
+
+const PORT= process.env.PORT || 4000;
+
+app.use(express.json());
+//middel ware for file upload
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
+const db = require('./config/database');
+db.connect();
+
+const cloudinary = require('./config/cloudinary');
+cloudinary.cloudinaryConnect();
+
+//api routes
+const Upload= require('./routes/FileUpload');
+app.use('/api/v1/upload', Upload);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
