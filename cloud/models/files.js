6ec9149f554e3fd7a99bde8ab39c73ef17453484
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-22 23:33:02 +0530

    update model file

---
+++ cloud/models/files.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const fileSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    imageUrl:{
+        type:String,
+        
+    },
+    tags:{
+        type:String,
+        
+    },
+    email:{
+        type:String,
+        
+    },
+})
+
+module.exports = mongoose.model('File', fileSchema);
