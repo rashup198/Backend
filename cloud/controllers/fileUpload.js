6cf3cf9794f5ab45379bf4e3d1f7c551ed7a1a86
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-23 22:20:14 +0530

    updated fileUpload.js

--- cloud/controllers/fileUpload.js
+++ cloud/controllers/fileUpload.js
@@ -1,4 +1,6 @@
+const { cloudinaryConnect } = require('../config/cloudinary');
 const File = require('../models/files');
+const cloudinary = require('cloudinary').v2;
 
 //local file upload
 
@@ -16,6 +18,61 @@ exports.localFileUpload = async (req, res) => {
             success:true,
             msg:"File uploaded successfully"
         })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// function which checks if the file type is supported
+function isFileTypeSupported(fileType, supportedTypes) {
+    return supportedTypes.includes(fileType);
+}
+
+
+// function which uploads the file to cloudinary
+async function uploadFileToCloudinary(file,folder){
+    const options= {folder}
+    console.log("temp file path",file.tempFilePath);
+    return await cloudinary.uploader.upload(file,tempFilePath,options)
+}
+
+// image upload to cloudinary
+exports.imageUpload= async(req,res)=>{
+    try {
+        //data fetch
+        const{name,tags,email}=req.body;
+        const file=req.files.imageFile;
+        console.log(file);
+
+        // validation for image
+        const supportedTypes = ['png', 'jpg', 'jpeg'];
+        const fileType = file.name.split('.')[1];
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success: false,
+                msg: 'File type not supported'
+            });
+        }
+
+        //upload image to cloudinary
+        console.log("uploading file to cloudinary");
+        const response= await uploadFileToCloudinary(file,"rashu");
+        console.log(response);
+
+        // save entry to db
+        const fileData= await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url
+        });
+        
+        res.json({
+            success:true,
+            msg:"Image uploaded successfully",
+            data:fileData
+        })
+        
     } catch (error) {
         console.log(error);
     }
